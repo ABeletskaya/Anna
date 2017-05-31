206b4f703b8110d7bd62fa24e122aa99f9611d2d
yleontyuk <bakst@yandex.ru>
2017-05-31 19:50:21 +0300

    ControlThread

---
+++ ControlThread/ControlThread/FileWatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ControlThread
+{
+    class FileWatcher
+    {
+        private string patch;
+
+
+        public event Action<DateTime> DataChanged;
+
+
+        public FileWatcher(string patch)
+        {
+            this.patch = patch;
+        }
+
+        private static readonly object syncObject = new object();
+
+        public void Start()
+        {
+            Thread t = new Thread(() =>
+            {
+                DateTime dt;
+                Console.WriteLine("Method Start");
+                // string path = @"..\..\test.txt";
+                if (!File.Exists(patch))
+                {
+                    File.Create(patch);
+                } else
+                {
+                    dt = File.GetLastWriteTime(patch);
+                    while (true)
+                    {
+                        if (dt != File.GetLastWriteTime(patch))
+                        {
+                            dt = File.GetLastWriteTime(patch);
+                            //Console.WriteLine("Last write time: {0}", dt);
+                            Console.WriteLine("data change");
+                            DataChanged?.Invoke(dt);
+                        }
+                    }
+                }
+            });
+            t.Start();
+        }
+    }
+}
---
+++ ControlThread/ControlThread/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ControlThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileWatcher observer = new FileWatcher(@"..\..\test.txt");
+            observer.DataChanged += TimeChanged;
+            observer.Start();
+        }
+
+        static void TimeChanged(DateTime data)
+        {
+            Console.WriteLine("Last write time: {0}", data);
+        }
+    }
+}
